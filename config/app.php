9eb3c4cb92cf0da347f39a92387c68777e98c5f7
Shift <shift@laravelshift.com>
2025-01-16 13:34:41 +0000

    Re-register service providers

--- config/app.php
+++ config/app.php
@@ -7,23 +7,6 @@ return [
 
     'track_page_views' => false,
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Package Service Providers...
-         */
-        \SocialiteProviders\Manager\ServiceProvider::class,
-
-        /*
-         * Application Service Providers...
-         */
-        \Spark\SparkServiceProvider::class,
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\SparkServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-    ])->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
