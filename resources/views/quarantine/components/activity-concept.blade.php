a1fb92e4ca6db052736b82477b41291aa3d0ebdf
R. Adrian <adrian@abdulbaha.xyz>
2025-12-10 17:20:46 +0100

    Remove quarantined legacy views from Git tracking.

--- resources/views/quarantine/components/activity-concept.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="activity-concept">
-    <img src="{{ asset('/img/star.svg') }}" alt="Bullet Point" width="48px"/>
-    {{--    <div style="font-size: 6.4rem">*</div>--}}
-    <div>
-        {{ $slot }}
-    </div>
-</div>
