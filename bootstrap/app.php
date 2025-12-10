62506900e0d0b40bc0e068dfc77fc39a9286d19c
R. Adrian <adrian@abdulbaha.xyz>
2025-12-10 17:56:31 +0100

    Allow Faker package to be used in non-dev environment (staging) to seed staging database.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,7 +41,7 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
-            if (! app()->environment(['local', 'testing']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
+            if (! app()->environment(['local']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
                 report($exception);
 
                 Inertia::setRootView('layouts.app');
